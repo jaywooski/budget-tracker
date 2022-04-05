1629b9271188d66d129bf16a6f082a1825f06672
Woogster <john.m.wooley@gmail.com>
2022-04-05 01:21:04 -0500

    Added .gitiginore whoopsies

---
+++ public/js/idb.js
@@ -0,0 +1,76 @@
+// Create variable to hold db connection
+let db;
+
+// establish the connection to IndexedDB, name it and set to version 1
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_record', { autoIncrement: true });
+  };
+
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+      // we haven't created this yet, but we will soon, so let's comment it out for now
+      // uploadBudget();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
+// Save data if no internet connection
+function saveData(data) {
+    // create new transaction with db
+    const record = db.record(['new_record'], 'readwrite');
+    const recordObjectStore = record.objectStore('new_record');
+
+    // add data to object store(table)
+    recordObjectStore.add(data);
+};
+
+function uploadData() {
+    const record = db.record(['new_record'], 'readwrite');
+    const recordObjectStore = record.objectStore('new_record');
+
+    // function to get all data
+    const getAll = recordObjectStore.getAll();
+
+    // On success of getAll function, upload
+    getAll.onsuccess = () => {
+        if(getAll.result.length > 0) {
+            fetch('api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then( data => data.json())
+            .then((data) => {
+                if(data.message) {
+                    throw new Error(data);
+                }
+                
+                const record = db.record(['new_record'], 'readwrite');
+                const recordObjectStore = record.objectStore('new_record');
+                recordObjectStore.clear();
+
+                alert('All saved records have been uploaded to your budget!!')
+
+
+            })
+            .catch(err => {console.log(err)})
+        }
+    }
+};
+
+window.addEventListener('online', uploadData);
---
+++ public/service-worker.js
@@ -0,0 +1,73 @@
+const APP_PREFIX = 'budget_app';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  '/',
+  './css/styles.css',
+  './index.html',
+  './js/idb.js',
+  './js/index.js',
+  './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function (cache) {
+
+                console.log(`Cache ${CACHE_NAME} is installing!`)
+        
+                return cache.addAll(FILES_TO_CACHE)
+            })
+    )
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(list) {
+            let keepList = list.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            
+            keepList.push(CACHE_NAME);
+
+            return Promise.all(
+                list.map(function(key, i) {
+                    if (keepList.indexOf(key) === -1) {
+
+                        console.log(`Cache ${list[i]} deleting`);
+                        return caches.delete(list[i]);
+                    }
+                })
+            )
+        })
+    )
+});
+
+
+self.addEventListener('fetch', function(e) {
+
+    e.respondWith(
+        caches.match(e.request).then((request)=> {
+            if(request){
+                return request
+            }
+            return fetch(e.request);
+        })
+    );
+});
+
+
+
+
+
