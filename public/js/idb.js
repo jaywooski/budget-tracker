b8e5c26a0dd97267f05a75edf542c0bc799e179b
Woogster <john.m.wooley@gmail.com>
2022-04-08 19:11:14 -0500

    idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,7 +8,7 @@ const request = indexedDB.open('budget_app', 1);
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_transaction', { autoIncrement: true });
   };
 
@@ -17,9 +17,10 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run uploadData() function to send all local db data to api
     if (navigator.onLine) {
-      uploadData();
+        console.log('online');
+        uploadData();
     }
 };
 
